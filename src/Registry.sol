d7b5f5f63b4985f6a0dde38f7e6c7f9199134670
hashedMae <mae@hashedmae.gay>
2022-04-26 14:32:00 -0700

    made recommended changes to Registry.sol

--- src/Registry.sol
+++ src/Registry.sol
@@ -13,31 +13,20 @@ contract Registry {
      mapping(string => address) public nameToUser;
 
     ///@dev events for tracking when names are registered or released
-    event NameRegistered(string name, address user);
-    event NameReleased(string name, address user);
+    event Registered(address user, string name);
+    event Released(address user, string name);
 
-    /**
-    @notice checks if a name is currently registered to a user
-    @dev for internal use only, not publicly callable
-    @param _name string that is being checked if it is registered
-    @return true if name is currently registered to a user, otherwise false
-     */
-     function _isRegistered(string memory _name) internal view returns(bool) {
-         if(nameToUser[_name] != address(0x0)){
-             return true;
-         }
-         return false;
-     }
+    
 
      /**
      @notice function to register a name
      @dev emits NameRegistered on success
      @param name string that is being registered
       */
-    function registerName(string memory name) public {
-        require(_isRegistered(name) == false, "name is already registered");
+    function registerName(string calldata name) external {
+        require(nameToUser[name] == address(0x0), "name is already registered");
         nameToUser[name] = msg.sender;
-        emit NameRegistered(name, msg.sender);
+        emit Registered(msg.sender, name);
     }
 
     /**
@@ -45,11 +34,10 @@ contract Registry {
     @dev emits NameReleased on success
     @param name string that is being released from ownership
      */
-    function releaseName(string memory name) public {
-        require(_isRegistered(name) == true, "name currently isn't registered");
+    function releaseName(string calldata name) external {
         require(nameToUser[name] == msg.sender, "name can only be released by current owner");
         nameToUser[name] = address(0x0);
-        emit NameReleased(name, msg.sender);
+        emit Released(msg.sender, name);
     }
 
     function checkName(string memory name) public view returns(address) {
